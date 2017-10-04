aeecba6ae63670ab1a525aad7165a1a7cc500804
MaliciousHex <reallifexamnesia@gmail.com>
2017-10-04 12:20:02 +0700

    fixing validation in index

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,7 +25,16 @@ class Welcome extends CI_Controller {
 
 	public function submitForm(){
 		$this->form_validation->set_rules('fname', 'Fullname', 'required',array('required'=>'Please Fill Your name'));
-	 	$this->form_validation->set_rules('pass', 'Password', 'required',array('required'=>'Please Fill Password'));
+	 	$this->form_validation->set_rules('femail', 'Email', 'required',array('required'=>'Please Fill Email'));
+	 	$this->form_validation->set_rules('fphone', 'Phone', 'required',array('required'=>'Please Fill Phone'));
+	 	$this->form_validation->set_rules('fmessage', 'Message', 'required',array('required'=>'Please Fill Message'));
+
+	 	if($this->form_validation->run()==false){
+	 		$this->load->view("thanks");
+	 	}else{
+	 		$this->load->view("index");
+	 	}
+
 
 
 	}
